94739e24743d9b48beee8508cf37afadf90d4fdc
mehregan zare <mehregan.zare7731@gmail.com>
2026-05-13 20:36:04 +0330

    Add task 1778691962_7388

---
+++ 1778691962_7388.py
@@ -0,0 +1,7 @@
+log('Executing recorded actions')
+log('Click at (1087,264)')
+move_mouse(1087, 264)
+page.mouse.click(1087, 264)
+wait_timeout(page, 0.3)
+browser.screenshot(page, 'latest_screenshot.png', full_page=False)
+with open('done_1778691962_7388.txt','w') as f: f.write('ok')
